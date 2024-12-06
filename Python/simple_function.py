cb3a034137247887c50bd0017ec1e2d2aaca0081
Akbar014 <akbar.cse47@gmail.com>
2024-12-06 22:28:17 +0600

    start in python

---
+++ Python/simple_function.py
@@ -0,0 +1,12 @@
+
+print("Hey! this is start with python")
+ 
+def avg(numbers):
+    mean = sum(numbers) / len(numbers)
+
+    return mean
+
+
+numbers = [1,2,3,4,5]
+result = avg(numbers)
+print(result)
