55626b42f9c8876677ffe0d907b2765f96c455b3
Akbar014 <akbar.cse47@gmail.com>
2024-12-25 20:12:29 +0600

    Some leetcode problem for binary problem

---
+++ CPP for DS/vector.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    
+
+vector<int>v1 = {1,2,3};
+vector<int>v2 = {1,3,2};
+
+if(v1==v2){
+    cout << "True" << endl;
+}else{
+    cout << "False" << endl;
+}
+
+    return 0;
+}
