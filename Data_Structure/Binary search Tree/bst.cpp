02ed7b4b5af4dbf32e5912539ca4cf88330eb02e
Akbar014 <akbar.cse47@gmail.com>
2024-12-28 23:45:12 +0600

    binary search tree

---
+++ Data_Structure/Binary search Tree/bst.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+        int val;
+        Node *left;
+        Node *right;
+        Node (int val){
+            this->val = val;
+            this->left = NULL;
+            this->right = NULL;
+        }
+};
+void take_input(Node * root, int val) 
+int main(){
+
+    return 0;
+}
